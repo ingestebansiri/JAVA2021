a98f952bffc63045fad2486383771ca9cc9d416c
Esteban <Esteban@192.168.1.2>
2021-08-27 20:13:10 -0300

    Prueba 1 opcionVentana commit

--- src/pruebagit3/PruebaGit3.java
+++ src/pruebagit3/PruebaGit3.java
@@ -16,8 +16,7 @@ public class PruebaGit3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-   System.out.println("esteban");
-   System.out.println("Franco");
+   System.out.println("nuevaPrueba");
     
     }
     
