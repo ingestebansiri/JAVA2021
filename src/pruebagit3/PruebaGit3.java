53ab70047b14af120d72d6efbefb42001b0877f3
Franco <Franco@DESKTOP-B25N62B>
2021-08-27 20:16:21 -0300

    pruebacommit

--- src/pruebagit3/PruebaGit3.java
+++ src/pruebagit3/PruebaGit3.java
@@ -17,7 +17,8 @@ public class PruebaGit3 {
     public static void main(String[] args) {
         // TODO code application logic here
    System.out.println("nuevaPrueba");
-    
+   
+     System.out.println("asdf");
     }
     
 }
